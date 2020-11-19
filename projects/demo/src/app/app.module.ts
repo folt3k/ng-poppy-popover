6317db9d247e0a40cabca9c5cc7110d7adf3713a
folt3k <kfoltyniak@catalogicsoftware.com>
2020-11-19 19:13:28 +0100

    header

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PopoverModule } from 'ng-poppy';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, PopoverModule],
+  imports: [BrowserModule, HttpClientModule, CoreModule, SharedModule, PopoverModule],
   providers: [],
   bootstrap: [AppComponent],
 })
