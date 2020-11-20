be537297b2f75bf20be3bed686784b629133e953
folt3k <kfoltyniak@catalogicsoftware.com>
2020-11-20 13:17:10 +0100

    deploy changes

---
+++ projects/demo/src/app/shared/demo-widget/demo-widget.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'demo-widget',
+  templateUrl: './demo-widget.component.html',
+  styleUrls: ['./demo-widget.component.scss'],
+})
+export class DemoWidgetComponent implements OnInit {
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- projects/demo/src/app/shared/shared.module.ts
+++ projects/demo/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CodeSnippetComponent } from './code-snippet/code-snippet.component';
+import { DemoWidgetComponent } from './demo-widget/demo-widget.component';
 
 @NgModule({
-  declarations: [CodeSnippetComponent],
+  declarations: [CodeSnippetComponent, DemoWidgetComponent],
   imports: [CommonModule],
-  exports: [CodeSnippetComponent],
+  exports: [CodeSnippetComponent, DemoWidgetComponent],
 })
 export class SharedModule {}
