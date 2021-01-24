79c7bc5c882cedfbeb4ac8af65fd7e4c088a2e3e
folt3k <kfoltyniak@catalogicsoftware.com>
2021-01-24 16:10:34 +0100

    menu module

--- projects/demo/src/app/pages/menu/menu.module.ts
+++ projects/demo/src/app/pages/menu/menu.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { SharedModule } from '../../shared/shared.module';
 import { BasicUsageComponent } from './examples/basic-usage/basic-usage.component';
+import { NestedMenuComponent } from './examples/nested-menu/nested-menu.component';
+import { ContextMenuComponent } from './examples/context-menu/context-menu.component';
 
 @NgModule({
-  declarations: [MenuComponent, BasicUsageComponent],
+  declarations: [MenuComponent, BasicUsageComponent, NestedMenuComponent, ContextMenuComponent],
   imports: [CommonModule, SharedModule],
   exports: [MenuComponent],
 })
